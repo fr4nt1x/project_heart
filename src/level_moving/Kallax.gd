2d20ff57ce0c0f2adceadf1a31a5b274b881bf32
Fr4nt1x <Fr4nt1x@Muc.de>
2023-03-16 18:51:59 +0100

    add boundaries

--- src/level_moving/Kallax.gd
+++ src/level_moving/Kallax.gd
@@ -4,20 +4,31 @@ extends Node2D
 # Y top
 # Z depth
 const step_x := Vector2(64,0)
-const step_y := Vector2(0,64)
-const step_z := Vector2(32,32)
+const step_y := Vector2(0,-64)
+const step_z := Vector2(-32,-16)
 
-var position_indices := [Vector3(0,0,0),Vector3(1,0,0),Vector3(2,0,0),Vector3(3,0,0)]
-
+var position_indices := [Vector3(0,0,0),Vector3(0,1,0),Vector3(0,2,0),Vector3(0,3,0)]
+onready var parent:=get_node("../")
 
 func _ready():
 	pass
-
-func move(direction):
 	
+
+	
+func move(direction):
+	var new_position_indices:=[]
+	var new_index:=Vector3()
 	for i in self.position_indices.size():
-		self.position_indices[i]+=direction
+		new_index= self.position_indices[i]+direction
+		new_position_indices.append(new_index)
+		if not parent._can_move(new_index):
+			return
+		
+	self.position_indices = new_position_indices
+		
 	#TODO vector dot product
 	self.position = (position_indices[0][0]* self.step_x + 
 						position_indices[0][1]* self.step_y+
 						position_indices[0][2] * self.step_z)
+	if not parent.can_move_in_z(self.position_indices):
+		print("next prop")#todo next prop
--- src/level_moving/PlayerMoving.gd
+++ src/level_moving/PlayerMoving.gd
@@ -1,13 +1,62 @@
 extends Node
 
-# For now the first child
-onready var current_prop = self.get_children()[0]
+# For now the last child
+onready var current_prop = self.get_children()[-1]
 
+const size_x := 6
+const size_y := 6
+const size_z := 10
+const step_x := Vector2(64,0)
+const step_y := Vector2(0,-64)
+const step_z := Vector2(-32,-16)
+var space_occupied_matrix:=[]
 
-
+func _build_debug_grid():
+	var scene = load("res://src/level_moving/Box.tscn")	
+	for x in range(size_x):
+		for y in range(size_y):
+			for z in range(size_z):
+				if space_occupied_matrix[x][y][z]:
+					var box = scene.instance()
+					box.position = Vector2(x*step_x)+Vector2(y*step_y)+Vector2(z*step_z)
+					add_child(box)
+func _init():
+	for x in range(size_x):
+		space_occupied_matrix.append([])
+		space_occupied_matrix[x] = []
+		for y in range(size_y):
+			space_occupied_matrix[x].append([])
+			space_occupied_matrix[x][y] = []
+			for z in range(size_z):
+				space_occupied_matrix[x][y].append([])
+				space_occupied_matrix[x][y][z]=false
+				
 func _ready():
-	pass
+	_build_debug_grid()
+
+func _can_move(position_index):
+	var can_move:=true
+	if ((position_index[0] <0 or position_index[0] >= size_x) or 
+		(position_index[1] <0 or position_index[1] >= size_y) or
+		(position_index[2] <0 or position_index[2] >= size_z)):
+		can_move=false
+	elif self.space_occupied_matrix[position_index[0]][position_index[1]][position_index[2]]:
+		can_move=false
+		
+	return can_move
+
+func can_move_in_z(position_indices):
+	for x in position_indices:
+		#keep order of comparisons
+		if x[2]+1>=self.size_z or self.space_occupied_matrix[x[0]][x[1]][x[2]+1]:
+			return false
+	return true
 	
+func update_space_occupied_matrix(position_indices):
+	for x in position_indices:
+		self.space_occupied_matrix[x[0]][x[1]][x[2]] = true
+	_build_debug_grid()
+		
 func _process(_delta):
 	var direction:=Vector3(0,0,0)
 	if Input.is_action_just_pressed("move_right"):
@@ -15,10 +64,10 @@ func _process(_delta):
 	elif Input.is_action_just_pressed("move_left"):
 		direction= Vector3(-1,0,0)
 	elif Input.is_action_just_pressed("jump"):
-		direction= Vector3(0,-1,0)
-	elif Input.is_action_just_pressed("move_down"):
 		direction= Vector3(0,1,0)
+	elif Input.is_action_just_pressed("move_down"):
+		direction= Vector3(0,-1,0)
 	elif Input.is_action_just_pressed("shoot"):
-		direction= Vector3(0,0,-1)
+		direction= Vector3(0,0,1)
 	if direction != Vector3(0,0,0):
-		current_prop.move(direction)	
+		current_prop.move(direction)
